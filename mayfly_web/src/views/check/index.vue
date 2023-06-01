1329130bcc448a37a3a5dbacb25fceebe39d4490
yifei.wang <2208136895@qq.com>
2023-06-01 16:27:44 +0800

    2023.6.1日 考核主页

--- mayfly_web/src/views/check/index.vue
+++ mayfly_web/src/views/check/index.vue
@@ -4,34 +4,40 @@
 
     <el-card>
       <el-progress type="circle"
-                   :percentage="50" />
+                   v-model="fb"
+                   :percentage="fb" />
       海安发布 </el-card>
     <el-card>
       <el-progress type="circle"
-                   :percentage="50" />
+                   v-model="bb"
+                   :percentage="bb" />
       海安播报 </el-card>
     <el-card>
       <el-progress type="circle"
-                   :percentage="50" />
+                   v-model="rm"
+                   :percentage="rm" />
       海安融媒 </el-card>
     <el-card>
       <el-progress type="circle"
-                   :percentage="50" />
+                   v-model="wd"
+                   :percentage="wd" />
       我的海安 </el-card>
     <el-card>
       <el-progress type="circle"
-                   :percentage="50" />
+                   v-model="yhh"
+                   :percentage="yhh" />
       悦海安 </el-card>
     <el-card>
       <el-progress type="circle"
-                   :percentage="50" />
+                   v-model="rb"
+                   :percentage="rb" />
       海安日报 </el-card>
     <el-button type="primary"
                @click="searchInfo">获取数据</el-button>
     <el-button type="primary"
                @click="enterView">立即上传</el-button>
   </el-space>
-
+  
 </template>
 <script lang="ts">
 import checkApi from '@/common/checkApi.ts';
@@ -45,6 +51,7 @@ export default {
         const route = useRoute();
         const state = reactive({
             fileList: [] as any,
+            fb:0,bb:0,rm:0,wd:0,yhh:0,rb:0
         });
         const enterView = () => {
             router.push({ path: '/upload' });
@@ -56,7 +63,25 @@ export default {
             const res = checkApi.searchImage();
             console.log(res);
             state.fileList = res;
+
+            for (let tp of state.fileList){
+              console.log(tp);
+            }
+
+            // for (let tp of state.fileList) {
+            //   switch(tp.imageType){
+            //     case '1': state.fb = 100;break;
+            //     case '2': state.bb = 100;break;
+            //     case '3': state.rm = 100;break;
+            //     case '4': state.wd = 100;break;
+            //     case '5': state.yhh = 100;break;
+            //     case '6': state.rb = 100;break;
+            //     default : break;
+            //   }
+            // }
+            // console.log(state.fb);
         };
+        
         return {
             ...toRefs(state),
             enterView,
